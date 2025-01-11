213a095e4807030ed8e3ef88bdf57eb9035caec7
Abinesh-Mathivanan <abineshmathivanan31@gmail.com>
2025-01-11 19:33:18 +0530

    just perplexity + accuracy

--- chapters/Chapter 5 - Pretraining/lossFunction.py
+++ chapters/Chapter 5 - Pretraining/lossFunction.py
@@ -3,3 +3,10 @@ import torch.nn.functional as F
 
 def loss_fn(logits, targets):
     return F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+
+def perplexity(loss):
+    return torch.exp(loss)
+
+def accuracy(logits, targets):
+    predictions = torch.argmax(logits, dim=-1)
+    return (predictions == targets).float().mean()
